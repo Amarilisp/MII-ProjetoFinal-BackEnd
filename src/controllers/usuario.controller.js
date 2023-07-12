80cff8997984af93d87eedc7f889c60cddf5d225
amarilis <amarilispequeno@gmail.com>
2023-07-11 23:47:46 -0300

    endpoints cadastrar e listar usuário

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -2,52 +2,76 @@ const { Usuario } = require("../models/usuario");
 
 class UsuarioController {
   async createOneUsuario(request, response) {
-    try {
-      const {
-        nome,
-        sobrenome,
-        genero,
-        dataNascimento,
-        CPF,
-        telefone,
-        email,
-        senha,
-        status,
-      } = request.body;
+    const {
+      nome,
+      sobrenome,
+      genero,
+      dataNascimento,
+      CPF,
+      telefone,
+      email,
+      senha,
+      status,
+    } = request.body;
 
-      const data = await Usuario.create({
-        nome,
-        sobrenome,
-        genero,
-        dataNascimento,
-        CPF,
-        telefone,
-        email,
-        senha,
-        status,
-      });
-
-      return response.status(201).send(data);
-    } catch (error) {
-      console.error(error.message);
-      return response.status(400).send;
-      {
-      }
+    if (!nome || !sobrenome || !dataNascimento || !CPF || !email || !senha) {
+      //criar if de campos obrigatorios - devolver erro 400
+      return response.status(400).send({ message: "Campo obrigatório" });
     }
-  }
-  async listUsuario(request, response) {
-    const data = await Usuario.findAll();
 
-    return response.status(200).send(data);
+    const validaCPF = await Usuario.findOne({
+      // chamando a constante e pedindo pra procurar no banco de dados se o CPF já existe.
+      where: { CPF },
+    });
+    //outro if para condições: cpf e email já estão cadastrados
+    if (validaCPF) {
+      return response.status(409).send({ message: "CPF já cadastrado!" });
+    } // se o cpf já existe, retornar o erro.
+
+    const validaEmail = await Usuario.findOne({
+      where: { email },
+    });
+    if (validaEmail) {
+      return response.status(409).send({ message: "Email já cadastrado!" });
+    }
+
+    const date = await Usuario.create({
+      nome,
+      sobrenome,
+      genero,
+      dataNascimento,
+      CPF,
+      telefone,
+      email,
+      senha,
+      status: "Ativo",
+    });
+
+    return response.status(201).send({
+      id: date.id,
+      nome: date.nome,
+      sobrenome: date.sobrenome,
+      CPF: date.CPF,
+      genero: date.genero,
+      dataNascimento: date.dataNascimento,
+      telefone: date.telefone,
+      email: date.email,
+      senha: date.senha,
+      status: date.status,
+    });
   }
+
+  // criar o login usuario
 
   async listOneUsuario(request, response) {
     const { id } = request.params;
     const data = await Usuario.findOne({
       where: { id },
-      attributes: ["cpf"],
+      attributes: { exclude: ["senha"] },
     });
-
+    if (!data) {
+      return response.status(404).send("Usuário não encontrado.");
+    }
     return response.status(200).send(data);
   }
 }
