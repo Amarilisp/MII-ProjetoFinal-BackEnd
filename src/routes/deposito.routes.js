57d488c61ca6faf8ce152d780fbf7537dc39a37c
amarilis <amarilispequeno@gmail.com>
2023-07-25 15:27:42 -0300

    deposito routes atualizado

--- src/routes/deposito.routes.js
+++ src/routes/deposito.routes.js
@@ -2,21 +2,21 @@ const { Router } = require("express");
 const {
   createOneDeposito,
   dadosDeposito,
-  // statusDeposito,
-  // listDeposito,
-  // listDepositoid,
-  // deleteDepositoid,
+  statusDeposito,
+  listDeposito,
+  listDepositoid,
+  deleteDepositoid,
 } = require("../controllers/deposito.controller");
 class DepositoRouter {
   routesFromDeposito() {
     // todas as rotas references ao usuario vão aparecer aqui - routesFromUsuario vai pro index
     const depositoRoutes = Router();
     depositoRoutes.post("/depositos", createOneDeposito);
-    depositoRoutes.patch("/depositos/id:", dadosDeposito);
-    // depositoRoutes.patch("/depositos/id:/status", statusDeposito);
-    // depositoRoutes.get("/depositos", listDeposito);
-    // depositoRoutes.get("/depositos/id:", listDepositoid);
-    // depositoRoutes.delete("/depositos/id:", deleteDepositoid);
+    depositoRoutes.patch("/depositos/:id", dadosDeposito);
+    depositoRoutes.patch("/depositos/:id/status", statusDeposito);
+    depositoRoutes.get("/depositos", listDeposito);
+    depositoRoutes.get("/depositos/:id", listDepositoid);
+    depositoRoutes.delete("/depositos/:id", deleteDepositoid);
 
     return depositoRoutes;
   }
